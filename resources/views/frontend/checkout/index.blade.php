3d9a11da0d3f7c90292221298773cc816d8d2e15
FahriMaulana2 <fahrimaulana9322@gmail.com>
2026-05-06 12:12:07 +0700

    update cart page

--- resources/views/frontend/checkout/index.blade.php
+++ resources/views/frontend/checkout/index.blade.php
@@ -90,7 +90,7 @@
 </div>
 
 <script>
-const ADMIN_PHONE = '6281234567890';
+const ADMIN_PHONE = ' 6283831520933';
 
 document.getElementById('whatsapp-checkout').addEventListener('click', async function() {
 
