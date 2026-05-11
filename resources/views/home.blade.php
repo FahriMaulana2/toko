e3bf0cc268769fb35bb8e7a87dca735ec7eff3ee
FahriMaulana2 <fahrimaulana9322@gmail.com>
2026-05-11 04:09:01 +0000

    May 11, 2026, 11:09 AM

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,9 +1,23 @@
 @extends('layouts.app')
 
+@section('title', 'Kiana Furniture')
+
 @section('content')
 
-<h1 class="text-5xl text-center py-20 font-bold">
-    HOME WEBSITE FURNITURE
-</h1>
+<section class="min-h-screen flex items-center justify-center bg-[#F8F5F1]">
+
+    <div class="text-center">
+
+        <h1 class="text-6xl font-bold text-[#8B5E3C] mb-6">
+            KianaFurniture
+        </h1>
+
+        <p class="text-gray-600 text-xl">
+            Website furniture berhasil kembali 🚀
+        </p>
+
+    </div>
+
+</section>
 
 @endsection
