7386972777e54a103fa36115b6345ad1c6c12545
FahriMaulana2 <fahrimaulana9322@gmail.com>
2026-05-11 03:14:34 +0000

    May 11, 2026, 10:14 AM

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,9 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+<h1 class="text-5xl text-center py-20 font-bold">
+    HOME WEBSITE FURNITURE
+</h1>
 
-                    {{ __('You are logged in!') }}
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
 @endsection
